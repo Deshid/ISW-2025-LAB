9d78a1b529011719f79928d5b3229472cdff2377
Paula Labra <paula.labra2101@alumnos.ubiobio.cl>
2025-04-21 12:22:43 -0400

    tarea3

---
+++ clase-3-main/src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import {getUsers,createUser,getUser, deleteUser} from "../controllers/user.controller.js"
+import {Router} from 'express';
+
+const router= Router();
+router.post("/",createUser);
+router.get("/all",getUsers);
+router.get("/:id",getUser);
+router.delete("/:id",deleteUser);
+export default router;
